1eb87c1e728ef001a5971be517f6de3639ae8f1e
Alex Kong <alex.lt.kong@connect.ust.hk>
2024-06-10 19:58:12 +0800

    another module

--- README.md
+++ README.md
@@ -1,17 +1,15 @@
-# Temperature and humidity monitor
+# Sensor data pipeline
 
-Display real-time temperature and relative humidity on an 8-digit 7-segment
-LED display (digital tube) from an SHT31 sensor
+Collect data from sensor and send the readings for further processing.
 
 ## Environment and dependency
 
-* `cURL`: `apt install libcurl4-gnutls-dev`
-* JSON: `apt install libjson-c-dev`
-* [libiotctrl](https://github.com/alex-lt-kong/libiotctrl): GPIO support
-* Enable `I2C interface` with `raspi-config`.
-* Check status of `I2C` device with `dmesg | grep i2c`.
-* `$SEVEN_SSD_TELEMETRY_ENDPOINT` should be set to a RESTful API endpoint if data collected need to be reported to a
-central server.
+- `cURL`: `apt install libcurl4-gnutls-dev`
+- JSON: `apt install libjson-c-dev`
+- [libiotctrl](https://github.com/alex-lt-kong/libiotctrl): GPIO support
+- Enable `I2C interface` with `raspi-config`.
+- Check status of `I2C` device with `dmesg | grep i2c`.
 
 ### Installation
+
 <img src="./assets/installation.jpg"></img>
--- src/module_temp_rh_display.c
+++ src/module_temp_rh_display.c
@@ -1,5 +1,6 @@
 #include "global_vars.h"
 #include "module.h"
+#include "module_common.c"
 #include "utils.h"
 
 #include <iotctrl/7segment-display.h>
@@ -27,53 +28,7 @@ struct DHT31Handle {
 };
 
 struct PostCollectionContext post_collection_init(const json_object *config) {
-  const json_object *root = config;
-  json_object *root_7sd;
-  json_object_object_get_ex(root, "7seg_display", &root_7sd);
-  json_object *root_7sd_data_pin_num;
-  json_object_object_get_ex(root_7sd, "data_pin_num", &root_7sd_data_pin_num);
-  json_object *root_7sd_clock_pin_num;
-  json_object_object_get_ex(root_7sd, "clock_pin_num", &root_7sd_clock_pin_num);
-  json_object *root_7sd_latch_pin_num;
-  json_object_object_get_ex(root_7sd, "latch_pin_num", &root_7sd_latch_pin_num);
-  json_object *root_7sd_chain_num;
-  json_object_object_get_ex(root_7sd, "chain_num", &root_7sd_chain_num);
-  json_object *root_7sd_gpiochip_path;
-  json_object_object_get_ex(root_7sd, "gpiochip_path", &root_7sd_gpiochip_path);
-
-  struct iotctrl_7seg_disp_connection conn;
-  conn.data_pin_num = json_object_get_int(root_7sd_data_pin_num);
-  conn.clock_pin_num = json_object_get_int(root_7sd_clock_pin_num);
-  conn.latch_pin_num = json_object_get_int(root_7sd_latch_pin_num);
-  conn.chain_num = json_object_get_int(root_7sd_chain_num);
-  conn.refresh_rate_hz = 500;
-  strncpy(conn.gpiochip_path, json_object_get_string(root_7sd_gpiochip_path),
-          PATH_MAX);
-  if (conn.data_pin_num == 0 || conn.clock_pin_num == 0 ||
-      conn.latch_pin_num == 0 || conn.chain_num == 0 ||
-      strlen(conn.gpiochip_path) == 0) {
-    SYSLOG_ERR("Some required values are not provided");
-  }
-  conn.refresh_rate_hz = 500;
-  syslog(LOG_INFO, "7segment display parameters:");
-  syslog(LOG_INFO, "data_pin_num: %d", conn.data_pin_num);
-  syslog(LOG_INFO, "clock_pin_num: %d", conn.clock_pin_num);
-  syslog(LOG_INFO, "latch_pin_num: %d", conn.latch_pin_num);
-  syslog(LOG_INFO, "chain_num: %d", conn.chain_num);
-  syslog(LOG_INFO, "refresh_rate_hz: %d", conn.refresh_rate_hz);
-  syslog(LOG_INFO, "gpiochip_path: %s", conn.gpiochip_path);
-  struct PostCollectionContext ctx = {.init_success = true, .context = NULL};
-  struct iotctrl_7seg_disp_handle *h;
-  if ((h = iotctrl_7seg_disp_init(conn)) == NULL) {
-    SYSLOG_ERR("iotctrl_7seg_disp_init() failed. Check stderr for "
-               "possible internal error messages");
-    ctx.init_success = false;
-    ctx.context = NULL;
-    return ctx;
-  }
-  ctx.context = h;
-  iotctrl_7seg_disp_turn_on_all_segments(h, 10);
-  return ctx;
+  return _post_collection_init(config);
 }
 
 int post_collection(struct CollectionContext *c_ctx,
